e9a4bcb0b68a11ed5116ed37149766edf95860f4
kona86 <okpalan86@gmail.com>
2021-02-22 04:47:36 +0000

    a

---
+++ freeCodeCamp/freeCodeCamp/learning-php/include/vars.php
@@ -0,0 +1,5 @@
+<?php 
+    $my_height_in_feet=6.4;
+    function say_hi($name){
+    echo "Hello, $name!";
+    }
--- freeCodeCamp/freeCodeCamp/learning-php/oop/Book.php
+++ freeCodeCamp/freeCodeCamp/learning-php/oop/Book.php
@@ -5,18 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Include Statement</title>
+    <title>Book</title>
 </head>
 
 <body>
     <?php
-    $title = "My post";
-    $author = "Nnamdi";
-    include "./article_header.php";
-    include "./vars.php";
+    class Book {
+        var $title;
+        var $author;
+        var $pages;
 
-    echo "Nnamdi is $my_height_in_feet. in feet.";
-    
+    }
     ?>
 </body>
 
