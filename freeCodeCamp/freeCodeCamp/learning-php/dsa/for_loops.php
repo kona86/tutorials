5aa430436abb389402697fa92420780629cc673d
kona86 <okpalan86@gmail.com>
2021-02-22 04:30:14 +0000

    a

--- freeCodeCamp/freeCodeCamp/learning-php/dsa/for_loops.php
+++ freeCodeCamp/freeCodeCamp/learning-php/dsa/for_loops.php
@@ -14,7 +14,10 @@
         echo "$i <br>";
     }
 
-    $
+    $luckyNumbers = array(4, 8, 14, 16, 23, 42);
+    for ($i = 0; $i < count($luckyNumbers); $i++) {
+        echo " the lucky numbers is: $luckyNumbers[$i] <br>";
+    }
     ?>
 </body>
 
