be6f2f7588375cbd71e3958e9e796018a9469213
kona86 <okpalan86@gmail.com>
2021-02-22 04:38:17 +0000

    a

--- freeCodeCamp/freeCodeCamp/include/include_statement.php
+++ freeCodeCamp/freeCodeCamp/include/include_statement.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Include Statement</title>
 </head>
+
 <body>
-    
+    <?php
+    $title = "My post";
+    $author = "Nnamdi";
+    include "./article_header.php";
+    ?>
 </body>
+
 </html>
