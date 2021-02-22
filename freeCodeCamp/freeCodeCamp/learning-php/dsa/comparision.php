6e0f905da75d6e68ec39c81a5ac437d5dbbb2702
kona86 <okpalan86@gmail.com>
2021-02-22 04:18:50 +0000

    a

--- freeCodeCamp/freeCodeCamp/learning-php/dsa/comparision.php
+++ freeCodeCamp/freeCodeCamp/learning-php/dsa/comparision.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <?php
+    echo max(3, 5);
+    echo "<br>";
+    function get_max($num1, $num2)
+    {
+        if ($num1 > $num2) return $num1;
+        else if ($num2 > $num1) return $num2;
+        else return $num1;
+    }
+    echo max(5, 5) === get_max(5, 5);
+    ?>
 </body>
+
 </html>
---
+++ freeCodeCamp/freeCodeCamp/learning-php/dsa/switch.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Switch Statement</title>
+</head>
+
+<body>
+    <form action="switch.php" method="post">
+        <input type="text" name="grade" id="">
+        <input type="submit" value="">
+    </form>
+    <?php
+    $grade = $_POST["grade"];
+    switch ($grade) {
+        case 'A':
+            echo "Amazing- You got an A";
+            break;
+        case 'B':
+            echo "Good Work ,keep praticing";
+            break;
+        case "C":
+        case "D":
+            echo "Keep praticing";
+            break;
+        default:
+            echo "You did not enter  a grade!";
+            break;
+    }
+    ?>
+</body>
+
+</html>:
